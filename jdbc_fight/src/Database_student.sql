0f213665eeb51887dc9626b82451087231c9c213
Vansh Gupta <vanshsja1512@gmail.com>
2025-06-27 20:51:45 +0530

    Updated sql
    Intruduced the concept of auto increment in the sql

--- jdbc_fight/src/Database_student.sql
+++ jdbc_fight/src/Database_student.sql
@@ -2,7 +2,7 @@ use students_db;
 
 CREATE TABLE student
 (
-   id int not null,
+   id int AUTO_INCREMENT ,
    age INT NOT NULL,
    first VARCHAR(255),
    last VARCHAR(255),
@@ -11,11 +11,11 @@ CREATE TABLE student
 
 
 
-insert into student(id, age, first, last)
-values(1,24,'baba', 'neo');
+insert into student(age, first, last)
+values(24,'baba', 'neo');
 
-insert into student(id, age, first, last)
-values(2,18,'rani', 'makinda');
+insert into student(age, first, last)
+values(18,'rani', 'makinda');
 
 select * from student s;
 select * from student s where first='baba';
@@ -23,7 +23,7 @@ update student s set first = "bada baba" where s.id =1;
 /** delete from student  where id=1; **/
 
 create table address (
-   id int not null,
+   id int AUTO_INCREMENT,
 	lane varchar(250),
 	street varchar(250),
 	city varchar(250),
@@ -33,8 +33,8 @@ create table address (
 	FOREIGN KEY (student_id) REFERENCES student(id)
 )
 
-insert into address(id, lane, street, city, state, student_id)  values(1, "lane 3", "Saraswati Vihar", "Doon", "UK", 1);
-insert into address(id, lane, street, city, state, student_id)  values(2, "kali ghaati", "baris wala pahad", "Chennai", "UG", 2);
+insert into address(lane, street, city, state, student_id)  values("lane 3", "Saraswati Vihar", "Doon", "UK", 1);
+insert into address(lane, street, city, state, student_id)  values("kali ghaati", "baris wala pahad", "Chennai", "UG", 2);
 
 select * from address;
 select * from student s , address a where a.student_id=s.id and s.id =1;
